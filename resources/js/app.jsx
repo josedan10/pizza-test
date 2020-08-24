cf6969af6405c724b1f5bc791f06403ccab8651d
José Daniel  Quintero <josedanq100@gmail.com>
2020-08-24 16:52:09 -0400

    feat: first app setuo with react

---
+++ resources/js/app.jsx
@@ -0,0 +1,13 @@
+require('./bootstrap');
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const app = (
+    <div>
+        <h1>My pizza app</h1>
+    </div>
+)
+
+ReactDOM.render(app, document.getElementById('app'))
