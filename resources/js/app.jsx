4ab2eabbde65fe4adfb481cb319cd6efadf5b1c0
José Daniel  Quintero <josedanq100@gmail.com>
2020-08-25 02:12:34 -0400

    fix: changed prettier options

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from "styled-components";
 
-import Nav from './components/nav';
-import StarredCarousel from './components/starred-carousel'
+import Nav from "./components/nav";
+import StarredCarousel from "./components/starred-carousel";
 
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-import theme from './theme'
+import theme from "./theme";
 
-library.add(fab, fas)
+library.add(fab, fas);
 
 const App = () => (
-  <div>
-    <ThemeProvider theme={theme}>
-      <Nav />
-      <StarredCarousel />
-    </ThemeProvider>
+    <div>
+        <ThemeProvider theme={theme}>
+        <Nav />
+            <StarredCarousel />
+      </ThemeProvider>
   </div>
-)
+);
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById("app"));
 
-require('./bootstrap');
+require("./bootstrap");
